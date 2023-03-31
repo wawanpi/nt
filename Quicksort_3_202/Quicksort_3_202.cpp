a9f530fc9a3a001e21901b911841654485b8974a
wawanpi <muhaguskurniawan@gmail.com>
2023-03-31 10:53:16 +0700

    number of comparasion

--- Quicksort_3_202/Quicksort_3_202.cpp
+++ Quicksort_3_202/Quicksort_3_202.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 
-
+//array of integres to hold valuse
+int arr[20];
+int camp_count = 0; //number of comparasion
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
