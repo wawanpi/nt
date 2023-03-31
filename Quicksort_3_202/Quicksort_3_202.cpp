03df5cd266cdd3554867f48beaf0fc2b807e9256
wawanpi <muhaguskurniawan@gmail.com>
2023-03-31 10:58:24 +0700

    display

--- Quicksort_3_202/Quicksort_3_202.cpp
+++ Quicksort_3_202/Quicksort_3_202.cpp
@@ -6,6 +6,19 @@ int arr[20];
 int camp_count = 0; //number of comparasion
 int move_count = 0; //number of movment
 int n;
+void input() {
+	while (true)
+	{
+		cout << "masukan panjang element array :";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else
+			cout << "\n Maksimum panjang array adalah 20" << endl;
+
+
+	}
+}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
