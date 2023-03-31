ba914d1a56b8ea308d28466e2fb5df529765733f
wawanpi <muhaguskurniawan@gmail.com>
2023-03-31 10:54:17 +0700

    number of movment

--- Quicksort_3_202/Quicksort_3_202.cpp
+++ Quicksort_3_202/Quicksort_3_202.cpp
@@ -4,6 +4,8 @@ using namespace std;
 //array of integres to hold valuse
 int arr[20];
 int camp_count = 0; //number of comparasion
+int move_count = 0; //number of movment
+int n;
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
