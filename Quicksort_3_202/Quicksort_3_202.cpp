9cc3f3ec58b131803b3186362459fea93388d342
wawanpi <muhaguskurniawan@gmail.com>
2023-03-31 19:09:43 +0700

    done

--- Quicksort_3_202/Quicksort_3_202.cpp
+++ Quicksort_3_202/Quicksort_3_202.cpp
@@ -39,7 +39,7 @@ void swap(int x, int y)
 }
 
 void q_short(int low, int high)
-{
+	{
 	int pivot, i, j;
 	if (low > high)                                                 // Langkah algoritma No. 1
 		return;
