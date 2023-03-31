6409b98dbe3fe333be5fc10ea8033b06be23a8d3
wawanpi <muhaguskurniawan@gmail.com>
2023-03-31 18:13:43 +0700

    langkah 11

--- Quicksort_3_202/Quicksort_3_202.cpp
+++ Quicksort_3_202/Quicksort_3_202.cpp
@@ -72,14 +72,10 @@ void q_short(int low, int high)
 			// swap the element at index i with the element at index j
 			swap(i, j);
 			mov_count++;
-
-
-
-
-
-
+		}
 }
-
+//j now containt the index of the last element in the sort list
+	if (low <j)//langkah11
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
